5191b3929b05a5e1c28ad68646f320902ddc6512
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-23 18:36:34 -0400

    adjust imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ProjectsContainer from '/containers/ProjectsContainer'
+import ProjectsContainer from './containers/ProjectsContainer'
 
 class App extends React.Component {
  
