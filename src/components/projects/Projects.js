e4a3f8b4dc8456a4667cd1873bb4807d36b48549
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-23 18:55:54 -0400

    add boilerplate code for projects

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Project from './Project'
+
+class Projects extends Component {
+
+    
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Projects
