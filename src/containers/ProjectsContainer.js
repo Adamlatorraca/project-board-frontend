d81a69aa08c5b15af0d56f1e5a0c0f9e90b8e324
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-23 17:45:09 -0400

    add projects container boilerplate code

---
+++ src/containers/ProjectsContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+// import ProjectInput from '../components/projects/ProjectInput
+// import Projects from '../components/projects/Projects
+
+class ProjectsComponent extends Component {
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsContainer)
