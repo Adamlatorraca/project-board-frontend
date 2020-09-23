d84e123baeffe74de54dbc443f92d68a51b4e0af
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-23 18:36:51 -0400

    add and remove imports

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import manageProject from './reducers/manageProject';
 
 const store = createStore(manageProject);
 
