9c87589f213dd8a86664aa82f8b474ab40cc3538
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:46:38 -0400

    tweak reducer to give projects an id

--- src/reducers/manageProject.js
+++ src/reducers/manageProject.js
@@ -1,19 +1,25 @@
 import uuid from 'uuid'
 
-function manageProjects(state = { projects: [] }, action) {
+function manageProjects(state = { projects: [], comments: [] }, action) {
 
 
     switch (action.type) {
         case 'ADD_PROJECT':
+            const project = {
+                id: uuid(),
+                title: action.project.title,
+                description: action.project.description,
+                contact: action.project.contact
+            }
             
 
-            return {...state, projects: [...state.projects, action.project] }
+            return {...state, projects: [...state.projects, project] }
     
         case 'ADD_COMMENT':
             const comment = {
                 id: uuid(),
                 projectId: action.comment.projectId,
-                text: action.project.text
+                text: action.comment.text
             }
 
             return {...state, comments: [state.comments, comment]}
