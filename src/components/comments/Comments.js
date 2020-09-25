e07c5cc75ee9ab763e779cd86903979cf3df7dc6
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:28:04 -0400

    add filertering and mapping of multiple comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -5,10 +5,15 @@ class Comments extends Component {
 
   render() {
 
+    const associatedComments = this.props.comments.filter(comment => comment.projectId === this.props.projectId)
+
+    const comments = associatedComments.map((comment, index) => {
+        return <Comment key={index} comment={comment} />
+    })
 
     return (
       <ul>
-
+          { comments }
       </ul>
     );
   }
