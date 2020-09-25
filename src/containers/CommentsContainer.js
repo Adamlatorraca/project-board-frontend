e52b703e552f844bdf2b24a8ad5fc100f718d0e0
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:46:16 -0400

    add comments prop to Comments

--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react'
 import CommentInput from '../components/comments/CommentInput'
-//import Comments from '../components/comments/Comments
+import Comments from '../components/comments/Comments'
 import { connect } from 'react-redux'
 
 class CommentsContainer extends Component {
@@ -9,6 +9,7 @@ class CommentsContainer extends Component {
         return(
             <div>
                 <CommentInput addComment={this.props.addComment} projectId={this.props.project.id}/>
+                <Comments comments={this.props.comments} projectId={this.props.project.id} />
             </div>
         )
     }
