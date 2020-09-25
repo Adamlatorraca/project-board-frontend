c1d64cfdf7ec15643cf680f79ac92f4a47cebbd7
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:28:57 -0400

    add comment boilerplate

---
+++ src/components/comments/Comment.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <li>
+        </li>
+      </div>
+    );
+  }
+
+};
+
+export default Comment;
