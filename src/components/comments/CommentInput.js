8f44403825a5453b44da9b9841263ee99b02181f
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:13:34 -0400

    add boilerplate for comment input

---
+++ src/components/comments/CommentInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class CommentInput extends Component {
+
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default CommentInput
