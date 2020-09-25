4d8fa82896a2709db5b2caf14ef5ce21b6c744e9
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:10:42 -0400

    add boilerplate for comments container

---
+++ src/containers/CommentsContainer.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react'
+//import CommentInput from '../components/comments/CommentInput'
+//import Comments from '../components/comments/Comments
+import { connect } from 'react-redux'
+
+class CommentsContainer extends Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default CommentsContainer
