3fee617cb7b76fec71d8b275a13325cfaeb9cb7d
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:48:30 -0400

    add redux debugging

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import manageProject from './reducers/manageProject';
 
-const store = createStore(manageProject);
+const store = createStore(manageProject, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
