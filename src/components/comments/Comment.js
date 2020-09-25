78514296d9af3dd055533fda9bb57eff6162a738
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:29:37 -0400

    render individual comments

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Comment extends Component {
 
   render() {
-
     return (
       <div>
         <li>
+            {this.props.comment.text}
         </li>
       </div>
     );
