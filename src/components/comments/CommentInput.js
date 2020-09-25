c28e0bdea60f9dbcd5378f199e7e29d6f6c9e305
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 10:16:55 -0400

    add form and onchange / onsubmit event listeners

--- src/components/comments/CommentInput.js
+++ src/components/comments/CommentInput.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react'
 
 class CommentInput extends Component {
 
+    state = {
+        text: ''
+    }
+
+    handleOnChange = event => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+        this.props.addComment({
+            text: this.state.text,
+            projectId: this.props.projectId
+        })
+        this.setState({
+            text: ''
+        })
+    }
+
 
     render() {
         return(
             <div>
-
+                <form onSubmit={this.handleOnSubmit}>
+                    <input type="text" value={this.state.text} onChange={this.handleOnChange} />
+                    <input type="submit" />
+                </form>
             </div>
         )
     }
